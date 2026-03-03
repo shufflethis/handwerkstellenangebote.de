fda2703a4d7cac6de2a806b0721b616c406bed3d
Autoblogger <dev@famefact.de>
2026-03-03 15:50:21 +0100

    feat: add HR contact forms + GEO authority metadata (HR-Updater v1.0)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
+// HR-UPDATER: v1.0
 import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { GeminiAdvisor } from './components/GeminiAdvisor';
 import { Home } from './pages/Home';
 import { Impressum } from './pages/Impressum';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import AutorSeite from './pages/AutorSeite';
+import Datenschutz from './pages/Datenschutz';
+import Impressum from './pages/Impressum';
+import Contact from './pages/Contact';
 
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState('home');
@@ -22,15 +28,23 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white font-sans text-slate-900">
+    
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div className="min-h-screen bg-white font-sans text-slate-900">
       <Header onNavigate={setCurrentPage} />
       <main>
         {renderPage()}
       </main>
       <GeminiAdvisor />
       <Footer onNavigate={setCurrentPage} />
-    </div>
-  );
+    </div>} />
+        <Route path="/kontakt" element={<Contact />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/datenschutz" element={<Datenschutz />} />
+        <Route path="/autor/thomas-sander" element={<AutorSeite />} />
+      </Routes>
+    </HashRouter>);
 };
 
 export default App;
